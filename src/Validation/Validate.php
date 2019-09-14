5452c8aa8a04edfea6690c57b6dfcdd6c55d7290
Ayman Elmalah <aymanelmalah77@gmail.com>
2019-09-14 03:52:34 +0300

    [#3] Fix Unique issue

--- src/Validation/Validate.php
+++ src/Validation/Validate.php
@@ -5,6 +5,7 @@ namespace Phplite\Validation;
 use Phplite\Http\Request;
 use Phplite\Session\Session;
 use Phplite\Url\Url;
+use Phplite\Validation\Rules\UniqueRule;
 use Rakit\Validation\Validator;
 
 class Validate {
@@ -24,8 +25,8 @@ class Validate {
     public static function validate(Array $rules, $json) {
         $validator = new Validator;
 
+        $validator->addValidator('unique', new UniqueRule());
         $validation = $validator->validate($_POST + $_FILES, $rules);
-
         $errors = $validation->errors();
 
         if ($validation->fails()) {
